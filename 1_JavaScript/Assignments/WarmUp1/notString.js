02ad38385db92fc41ed7d47921621a6e2285a123
Belvin-04 <belvinnadar527@gmail.com>
2022-07-04 19:10:49 +0530

    solved notString

---
+++ 1_JavaScript/Assignments/WarmUp1/notString.js
@@ -0,0 +1,20 @@
+// Warmup-1 -- notString
+// Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+
+// Examples
+
+// notString('candy') → not candy
+// notString('x') → not x
+// notString('not bad') → not bad
+
+
+function notString(str){
+    if(str.substring(0,3) == "not"){
+      return str;
+    }
+    return "not "+str;
+  }
+
+console.log(notString("candy"));
+console.log(notString("x"));
+console.log(notString("not bad"));

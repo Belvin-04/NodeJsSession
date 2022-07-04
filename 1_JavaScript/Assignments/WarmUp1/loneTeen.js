0df9576e34c3b0d9f597745879931b0ba9eb6aad
Belvin-04 <belvinnadar527@gmail.com>
2022-07-04 20:31:41 +0530

    solved loneTeen

---
+++ 1_JavaScript/Assignments/WarmUp1/loneTeen.js
@@ -0,0 +1,20 @@
+// Warmup-1 -- loneTeen
+// We'll say that a number is 'teen' if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+
+// Examples
+
+// loneTeen(13, 99) → true
+// loneTeen(21, 19) → true
+// loneTeen(13, 13) → false
+
+
+function loneTeen(a, b){
+    let count = 0;
+    if(a <= 19 && a>= 13){count++;}
+    if(b <= 19 && b>= 13){count++;}
+    return count == 1;
+  }
+
+  console.log(loneTeen(13,99));
+  console.log(loneTeen(21,19));
+  console.log(loneTeen(13,13));
